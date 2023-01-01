83c604907ed8781ed49625ed8ae4a7e4fa814ea8
DrelezTM <72683265+DrelezTM@users.noreply.github.com>
2022-12-31 16:27:40 -0800

    isGroups added

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, LocalAuth } = require('whatsapp-web.js');
+const { Client, LocalAuth, Chat } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 const moment = require('moment-timezone');
 const colors = require('colors');
@@ -18,7 +18,7 @@ client.on('qr', (qr) => {
     console.log(`[${moment().tz(config.timezone).format('HH:mm:ss')}] Scan the QR below : `);
     qrcode.generate(qr, { small: true });
 });
- 
+
 client.on('ready', () => {
     console.clear();
     const consoleText = './config/console.txt';
@@ -30,38 +30,43 @@ client.on('ready', () => {
             console.log(data.green);
             console.log(`[${moment().tz(config.timezone).format('HH:mm:ss')}] ${config.name} is Already!`.green);
         }
-    })
+    });
 });
 
 client.on('message', async (message) => {
-    if (message.type == "image") {
-        client.sendMessage(message.from, "*[⏳]* Loading..");
-        try {
-            const media = await message.downloadMedia();
-            client.sendMessage(message.from, media, {
-                sendMediaAsSticker: true,
-                stickerName: config.name, // Sticker Name = Edit in 'config/config.json'
-                stickerAuthor: `https://wa.me/${message.to.replace('@c.us', '')}` // Sticker Author = Your Whatsapp BOT Number
-            }).then(() => {
-                client.sendMessage(message.from, "*[✅]* Successfully!");
+    const isGroups = message.from.endsWith('@g.us') ? true : false;
+    if ((isGroups && config.groups) || !isGroups) {
+        if (message.type == "image") {
+            client.sendMessage(message.from, "*[⏳]* Loading..");
+            try {
+                const media = await message.downloadMedia();
+                client.sendMessage(message.from, media, {
+                    sendMediaAsSticker: true,
+                    stickerName: config.name, // Sticker Name = Edit in 'config/config.json'
+                    stickerAuthor: config.author // Sticker Author = Edit in 'config/config.json'
+                }).then(() => {
+                    client.sendMessage(message.from, "*[✅]* Successfully!");
+                });
+            } catch {
+                client.sendMessage(message.from, "*[❎]* Failed!");
+            }
+        } else if (message.type == "sticker") {
+            client.sendMessage(message.from, "*[⏳]* Loading..");
+            try {
+                const media = await message.downloadMedia();
+                client.sendMessage(message.from, media).then(() => {
+                    client.sendMessage(message.from, "*[✅]* Successfully!");
+                });  
+            } catch {
+                client.sendMessage(message.from, "*[❎]* Failed!");
+            }
+        } else {
+            client.getChatById(message.id.remote).then(async (chat) => {
+                await chat.sendSeen();
             });
-        } catch {
-            client.sendMessage(message.from, "*[❎]* Failed!");
-        }
-    } else if (message.type == "sticker") {
-        client.sendMessage(message.from, "*[⏳]* Loading..");
-        try {
-            const media = await message.downloadMedia();
-            client.sendMessage(message.from, media).then(() => {
-                client.sendMessage(message.from, "*[✅]* Successfully!");
-            });  
-        } catch {
-            client.sendMessage(message.from, "*[❎]* Failed!");
         }
     } else {
-        client.getChatById(message.id.remote).then(async (chat) => {
-            await chat.sendSeen();
-        });
+        client.sendMessage(message.from, 'groups verified')
     }
 });
 
