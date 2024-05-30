fdae16591da71d5ac2a2a82218e56ad672c5395a
DrelezTM <dreleztm@gmail.com>
2024-05-30 20:32:18 +0700

    Deleting comments

--- index.js
+++ index.js
@@ -10,13 +10,8 @@ const client = new Client({
         headless: true,
         args: [ '--no-sandbox', '--disable-setuid-sandbox' ]
     },
-    // webVersionCache: {
-    //     type: 'remote',
-    //     // remotePath: `https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2407.3.html`,
-    // },
     webVersionCache: { 
         type: 'remote', 
-        // remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2403.2.html', 
         remotePath: "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
     },
     ffmpeg: './ffmpeg.exe',
