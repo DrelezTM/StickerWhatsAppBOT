e401024ffe1761b63b4735d539ee88db3a91e7ad
DrelezTM <dreleztm@gmail.com>
2025-05-17 18:03:19 +0700

    [05/17/2025] Fix error on started

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const client = new Client({
     },
     webVersionCache: { 
         type: 'remote', 
-        remotePath: "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
+        remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2403.2.html'
     },
     ffmpeg: './ffmpeg.exe',
     authStrategy: new LocalAuth({ clientId: "client" })
